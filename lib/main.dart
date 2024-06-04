f424beb6137bee8eb73812f93c86007820edec77
BSC_INF_17_19 <bsc -inf-17-19@unima.ac.mw>
2024-06-04 21:38:16 +0200

    The project is connected to the firebase after failing to use https to fetch data from arduino

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
-//import 'package:smart_irrigation_system/statistics.dart';
+import 'esp_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+// ...
+
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+     await Firebase.initializeApp(
+       options: DefaultFirebaseOptions.currentPlatform,
+     );
   runApp(MyApp());
 }
 
@@ -26,21 +34,44 @@ class IrrigationPage extends StatefulWidget {
 }
 
 class _IrrigationPageState extends State<IrrigationPage> {
-  // Define variables to hold temperature and moisture values
-  String temperature = '73';
-  String moisture = '63%';
-  String humidity = '40%';
-  String sunlight = 'low';
-  String battery = '88%';
-  String irrigationStatus = 'ON';
-  final TextEditingController _textFieldController = TextEditingController();
+  //String temperature = 'N/A';
+  String moisture = 'N/A';
+  // String humidity = 'N/A';
+  // String sunlight = 'N/A';
+  // String battery = 'N/A';
+  // String irrigationStatus = 'N/A';
 
-  // Method to update temperature and moisture values
-  void updateValues(String temp,String irriStatus, String batt, String moist, String hum, String sun) {
-    setState(() {
-      temperature = temp; moisture = moist; humidity = hum; sunlight = sun;battery = batt;
-      irrigationStatus= irriStatus;
-    });
+  final TextEditingController _textFieldController = TextEditingController();
+  final ESPService espService = ESPService('http://192.168.137.178');
+
+  @override
+  void initState() {
+    super.initState();
+    getMoistureData();
+  }
+
+  Future<void> getMoistureData() async {
+    try {
+      final data = await espService.fetchData();
+      setState(() {
+        // temperature = data['temperature'].toString();
+        moisture = data['moisture'].toString();
+        // humidity = data['humidity'].toString();
+        // sunlight = data['sunlight'].toString();
+        // battery = data['battery'].toString();
+        // irrigationStatus = data['irrigationStatus'].toString();
+      });
+    } catch (e) {
+      print("Error in fetching data: $e");
+      setState(() {
+        // temperature = 'Error';
+        moisture = 'Error';
+        // humidity = 'Error';
+        // sunlight = 'Error';
+        // battery = 'Error';
+        // irrigationStatus = 'Error';
+      });
+    }
   }
 
   @override
@@ -79,44 +110,52 @@ class _IrrigationPageState extends State<IrrigationPage> {
         centerTitle: true,
         backgroundColor: Color(0xFFD9D9D9),
       ),
-      body: Container(width: 853, height: 953,
+      body: Container(
+        width: 853,
+        height: 953,
         clipBehavior: Clip.antiAlias,
         decoration: BoxDecoration(color: Color(0x931E3D6C)),
         child: Stack(
           children: [
-            Positioned(
-  left: 65, // Adjust the left position to move the icon to the right
-  top: 230, // Adjust the top position as needed
-  child: Icon(Icons.thermostat, color: Colors.white, size: 90), // Adjust size as needed
-),
-Positioned(
-  left: 51,
-  top: 315, // Adjust the top position of the text to move it below the icon
-  child: Text(
-    'Temperature: $temperature°C',
-    style: TextStyle(
-      color: Colors.white.withOpacity(0.7),
-      fontSize: 16, fontFamily: 'Inter',
-      fontWeight: FontWeight.w400, height: 0,
-    ),
-  ),
-),
-Positioned(
-    left: 100,
-    top: 165,
-    child: Text(
-      "IRRIGATION STATUS: $irrigationStatus", style: TextStyle(color: Colors.white.withOpacity(0.7),
-    fontSize: 17, fontFamily: 'Inter', fontWeight: FontWeight.w400, height: 0,),
-    )),
-
-            // Statistics Button
+            // Positioned(
+            //   left: 65,
+            //   top: 230,
+            //   child: Icon(Icons.thermostat, color: Colors.white, size: 90),
+            // ),
+            // Positioned(
+            //   left: 51,
+            //   top: 315,
+            //   child: Text(
+            //     'Temperature: $temperature°C',
+            //     style: TextStyle(
+            //       color: Colors.white.withOpacity(0.7),
+            //       fontSize: 16,
+            //       fontFamily: 'Inter',
+            //       fontWeight: FontWeight.w400,
+            //       height: 0,
+            //     ),
+            //   ),
+            // ),
+            // Positioned(
+            //   left: 100,
+            //   top: 165,
+            //   child: Text(
+            //     "IRRIGATION STATUS: $irrigationStatus",
+            //     style: TextStyle(
+            //       color: Colors.white.withOpacity(0.7),
+            //       fontSize: 17,
+            //       fontFamily: 'Inter',
+            //       fontWeight: FontWeight.w400,
+            //       height: 0,
+            //     ),
+            //   ),
+            // ),
             Positioned(
               left: 245,
-              top: 100, // Adjust this value to position the button
+              top: 100,
               child: ElevatedButton(
                 key: Key('statistics_button_key'),
                 onPressed: () {
-                  // Navigate to the next page
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => StatisticsPage()),
@@ -129,128 +168,131 @@ Positioned(
                 child: Text('Statistics'),
               ),
             ),
-
             Positioned(
-  left: 51,
-  top: 100, // Adjust the top position of the text field
-  child: Container(
-    width: 170, // Adjust the width of the container
-    child: Row(
-      children: [
-        Expanded(
-          child: TextField(
-            key: Key('crops_textField'),
-            controller: _textFieldController,
-            decoration: InputDecoration(hintText: 'Choose crop',
-              hintStyle: TextStyle(color: Colors.black.withOpacity(0.2)), // Adjust opacity as needed
-              fillColor: Colors.white, filled: true,
-              contentPadding: EdgeInsets.symmetric(horizontal: 10), // Adjust padding as needed
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8.0),
-                borderSide: BorderSide.none,
+              left: 51,
+              top: 100,
+              child: Container(
+                width: 170,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        key: Key('crops_textField'),
+                        controller: _textFieldController,
+                        decoration: InputDecoration(
+                          hintText: 'Choose crop',
+                          hintStyle: TextStyle(color: Colors.black.withOpacity(0.2)),
+                          fillColor: Colors.white,
+                          filled: true,
+                          contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                        onChanged: (value) {
+                          // Handle text field changes
+                        },
+                      ),
+                    ),
+                    PopupMenuButton<String>(
+                      icon: Icon(Icons.keyboard_arrow_down, color: Colors.white),
+                      itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                        PopupMenuItem<String>(
+                          value: 'Maize',
+                          child: Text('Maize'),
+                        ),
+                        PopupMenuItem<String>(
+                          value: 'Onion',
+                          child: Text('Onion'),
+                        ),
+                      ],
+                      onSelected: (String value) {
+                        _textFieldController.text = value;
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
-            onChanged: (value) {
-              // Handle text field changes
-            },
-          ),
-        ),
-         PopupMenuButton<String>(
-          icon: Icon(Icons.keyboard_arrow_down, color: Colors.white), // Add down arrow icon
-          itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-            PopupMenuItem<String>(
-              value: 'Maize',
-              child: Text('Maize'),
-            ),
-            PopupMenuItem<String>(
-              value: 'Onion',
-              child: Text('Onion'),
-            ),
-          ],
-          onSelected: (String value) {
-            _textFieldController.text = value; // Update text field value when item is selected
-          },
-        ),
-      ],
-    ),
-  ),
-),
-
+            // Positioned(
+            //   left: 255,
+            //   top: 240,
+            //   child: Icon(Icons.wb_sunny, color: Colors.white, size: 78),
+            // ),
+            // Positioned(
+            //   left: 245,
+            //   top: 315,
+            //   child: Text(
+            //     'Sunlight: $sunlight',
+            //     style: TextStyle(
+            //       color: Colors.white.withOpacity(0.7),
+            //       fontSize: 16,
+            //       fontFamily: 'Inter',
+            //       fontWeight: FontWeight.w400,
+            //       height: 0,
+            //     ),
+            //   ),
+            // ),
+            // Positioned(
+            //   left: 251,
+            //   top: 380,
+            //   child: Icon(Icons.opacity, color: Colors.white, size: 78),
+            // ),
+            // Positioned(
+            //   left: 245,
+            //   top: 463,
+            //   child: Text(
+            //     'Humidity: $humidity%',
+            //     style: TextStyle(
+            //       color: Colors.white.withOpacity(0.7),
+            //       fontSize: 16,
+            //       fontFamily: 'Inter',
+            //       fontWeight: FontWeight.w400,
+            //       height: 0,
+            //     ),
+            //   ),
+            // ),
+            // Positioned(
+            //   left: 151,
+            //   top: 530,
+            //   child: Column(
+            //     crossAxisAlignment: CrossAxisAlignment.start,
+            //     children: [
+            //       Icon(Icons.battery_5_bar_outlined, color: Colors.white, size: 90),
+            //       SizedBox(height: 10),
+            //       Text(
+            //         'Battery: $battery%',
+            //         style: TextStyle(
+            //           color: Colors.white.withOpacity(0.7),
+            //           fontSize: 16,
+            //           fontFamily: 'Inter',
+            //           fontWeight: FontWeight.w400,
+            //           height: 0,
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
             Positioned(
-  left: 255,
-  top: 240, // Adjust the top position of the icon
-  child: Icon(Icons.wb_sunny, color: Colors.white, size: 78), // Adjust size as needed
-),
-Positioned(
-  left: 245,
-  top: 315, // Adjust the top position of the text
-  child: Text(
-    'Sunlight: $sunlight',
-    style: TextStyle(
-      color: Colors.white.withOpacity(0.7), fontSize: 16, fontFamily: 'Inter',
-      fontWeight: FontWeight.w400, height: 0,
-    ),
-  ),
-),
-
+              left: 80,
+              top: 380,
+              child: Icon(Icons.water, color: Colors.white, size: 78),
+            ),
             Positioned(
-  left: 251,
-  top: 380, // Adjust the top position of the icon
-  child: Icon(Icons.opacity, color: Colors.white, size: 78), // Adjust size as needed
-),
-Positioned(
-  left: 245,
-  top: 463, // Adjust the top position of the text
-  child: Text(
-    'Humidity: $humidity%',
-    style: TextStyle(
-      color: Colors.white.withOpacity(0.7), fontSize: 16, fontFamily: 'Inter',
-      fontWeight: FontWeight.w400, height: 0,
-    ),
-  ),
-),
-
-          Positioned(
-  left: 151,
-  top: 530, // Adjust the top position as needed
-  child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Positioned(
-        child: Icon(Icons.battery_5_bar_outlined, color: Colors.white, size: 90,),
-      ),
-      SizedBox(height: 10), // Add some space between the icon and the battery percentage
-      Positioned(
-        child: Text(
-          'Battery: $battery%',
-          style: TextStyle(
-            color: Colors.white.withOpacity(0.7), fontSize: 16, fontFamily: 'Inter',
-            fontWeight: FontWeight.w400, height: 0,
-          ),
-        ),
-      ),
-    ],
-  ),
-),
-
-
-          Positioned(
-  left: 80,
-  top: 380, // Adjust the top position of the icon
-  child: Icon(Icons.water, color: Colors.white, size: 78), // Adjust size as needed
-),
-Positioned(
-  left: 51,
-  top: 463, // Adjust the top position of the text
-  child: Text(
-    'Soil Moisture: $moisture',
-    style: TextStyle(
-      color: Colors.white.withOpacity(0.7), fontSize: 16, fontFamily: 'Inter', fontWeight: FontWeight.w400,
-      height: 0,
-    ),
-  ),
-),
-
+              left: 51,
+              top: 463,
+              child: Text(
+                'Soil Moisture: $moisture',
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.7),
+                  fontSize: 16,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w400,
+                  height: 0,
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -258,7 +300,6 @@ Positioned(
   }
 }
 
-// Add StatisticsPage class
 class StatisticsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
